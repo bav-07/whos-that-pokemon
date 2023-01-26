01f5d14aa2485df2d621104bcae237027e69ac24
Bavaharsan Nagarajah <bavaharsan.nagarajah@gmail.com>
2023-01-26 20:01:22 +0000

    updated to accept pokemon based on default name and no longer requires hyphens in user input

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 let response;
 let jsonData; 
+let response2;
+let jsonData2;
 
 
 const fetchPokemon = async () => {
 
     document.querySelector("#feedback").textContent = ""
-
+    const image = document.querySelector("#pokemon-image");
     // console.log("pokemon");
     const dexId = Math.floor(Math.random() * 905) + 1;
+    
 
     response = await fetch(`https://pokeapi.co/api/v2/pokemon/${dexId}`);
     jsonData = await response.json();
     // console.log(jsonData);
-    const image = document.querySelector("#pokemon-image");
-    image.src = await jsonData.sprites.other["official-artwork"]["front_default"];
+    image.src = jsonData.sprites.other["official-artwork"]["front_default"];
     image.classList.add("notFound");
+
+    response2 = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${dexId}`)
+    jsonData2 = await response2.json();
+
+    console.log(jsonData2)
 
 }
 
@@ -30,7 +37,7 @@ const handleGuess = (event) => {
     const input = event.target.guess.value.toLowerCase();
     
     if (input !== "") {
-        if (input === jsonData.name){
+        if (input === jsonData2.name){
             console.log("correct");
             document.querySelector("#pokemon-image").classList.remove("notFound");
             document.querySelector("#feedback").textContent = `That's right!`
